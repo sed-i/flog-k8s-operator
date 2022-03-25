a370c0b279595627ac4d0ed6bc6a38bf7d4125ec
sed-i <82407168+sed-i@users.noreply.github.com>
2022-03-24 21:16:14 -0400

    Add pebble layer

--- src/charm.py
+++ src/charm.py
@@ -10,7 +10,8 @@ import logging
 
 from ops.charm import CharmBase
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, BlockedStatus, ModelError
+from ops.pebble import ChangeError, Layer
 
 logger = logging.getLogger(__name__)
 
@@ -27,7 +28,42 @@ class FlogCharm(CharmBase):
 
         Learn more about Pebble layers at https://github.com/canonical/pebble
         """
-        self.unit.status = ActiveStatus()
+        try:
+            self._update_layer()
+        except (ModelError, TypeError, ChangeError) as e:
+            self.unit.status = BlockedStatus("Layer update failed: {}".format(str(e)))
+        else:
+            self.unit.status = ActiveStatus()
+
+    def _flog_layer(self) -> Layer:
+        """Returns Pebble configuration layer for flog."""
+
+        def command():
+            return "/bin/flog --format rfc5424 --loop --delay 1s --type log --output /bin/fake.log"
+
+        return Layer(
+            {
+                "summary": "flog layer",
+                "description": "pebble config layer for flog",
+                "services": {
+                    "flog": {
+                        "override": "replace",
+                        "summary": "flog service",
+                        "command": command(),
+                        "startup": "enabled",
+                    }
+                },
+            }
+        )
+
+    def _update_layer(self):
+        container = self.unit.get_container("workload")  # container name from metadata.yaml
+        plan = container.get_plan()
+        overlay = self._flog_layer()
+
+        if overlay.services != plan.services:
+            container.add_layer("flog layer", overlay, combine=True)
+            container.replan()
 
 
 if __name__ == "__main__":
