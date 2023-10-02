cf75197485ed69fa2f098acd64b7ea54d9db32c9
sed-i <82407168+sed-i@users.noreply.github.com>
2023-10-02 12:55:29 -0400

    Fix itest

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -30,16 +30,12 @@ async def test_build_and_deploy(ops_test: OpsTest):
         charm, resources=resources, application_name=APP_NAME, series="focal"
     )
 
-    # issuing dummy update_status just to trigger an event
-    await ops_test.model.set_config({"update-status-hook-interval": "10s"})
-
     await ops_test.model.wait_for_idle(
         apps=[APP_NAME],
         status="active",
         raise_on_blocked=True,
         timeout=1000,
     )
-    assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
 
     # effectively disable the update status from firing
     await ops_test.model.set_config({"update-status-hook-interval": "60m"})
@@ -50,7 +46,10 @@ async def test_application_is_up(ops_test: OpsTest):
     status = await ops_test.model.get_status()  # noqa: F821
     address = status["applications"][APP_NAME]["units"][f"{APP_NAME}/0"]["address"]
 
-    url = f"http://{address}"
+    # Promtail's default HTTP listen port is 9080
+    # https://grafana.com/docs/loki/latest/send-data/promtail/configuration/
+    url = f"http://{address}:9080"
+    # Response is: <a href="/targets">See Other</a>.
 
     logger.info("querying app address: %s", url)
     response = urllib.request.urlopen(url, data=None, timeout=2.0)
